967bce9a204782b2be79906b0c001a352be50f4c
Yagiz Nizipli <yagiz@nizipli.com>
2024-01-05 15:53:51 -0500

    add diagnostic traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod traits;
+
+/// Default severity for diagnostics is `Severity::Error`.
+#[derive(Copy, Clone, Debug, Eq, PartialOrd, PartialEq, Ord)]
+pub enum Severity {
+    Warning,
+    Error,
+    Info,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Default for Severity {
+    fn default() -> Self {
+        Self::Error
     }
 }
+
+/// A span with an optional label.
+/// TODO(@anonrig): Why don't we merge this with SourceSpan and make the label
+/// optional?
+pub struct LabeledSourceSpan {
+    label: Option<String>,
+    span: SourceSpan,
+}
+
+/// Represents the start and length of the span.
+pub struct SourceSpan {
+    offset: SourceOffset,
+    length: usize,
+}
+
+/// Represents the offset from the beginning.
+pub struct SourceOffset(usize);

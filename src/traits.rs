2fd0a01d9e679d2abc1c56fe229be07d4d6e8b6e
Yagiz Nizipli <yagiz@nizipli.com>
2024-01-05 23:25:55 -0500

    add diagnostic traits (#1)

---
+++ src/traits.rs
@@ -0,0 +1,40 @@
+use crate::{LabeledSourceSpan, Severity};
+use std::{error::Error, fmt::Display};
+
+/// Ref: https://docs.rs/miette/latest/src/miette/protocol.rs.html#20-70
+pub trait Diagnostic: Error {
+    /// A unique identifier/code for the error.
+    fn code<'a>(&'a self) -> Option<Box<dyn Display + 'a>> {
+        None
+    }
+
+    /// The severity of the error.
+    fn severity(&self) -> Option<Severity> {
+        None
+    }
+
+    /// Additional help text for the error.
+    fn help<'a>(&'a self) -> Option<Box<dyn Display + 'a>> {
+        None
+    }
+
+    /// Labels to apply to this `Diagnostic`'s [`Diagnostic::source_code`]
+    fn labels(&self) -> Option<Box<dyn Iterator<Item = LabeledSourceSpan> + '_>> {
+        None
+    }
+
+    /// Additional related `Diagnostic`s.
+    fn related<'a>(&'a self) -> Option<Box<dyn Iterator<Item = &'a dyn Diagnostic> + 'a>> {
+        None
+    }
+
+    /// URL to visit for a more detailed description of the error.
+    fn url<'a>(&'a self) -> Option<Box<dyn Display + 'a>> {
+        None
+    }
+
+    /// The cause of the error.
+    fn diagnostic_source(&self) -> Option<&dyn Diagnostic> {
+        None
+    }
+}

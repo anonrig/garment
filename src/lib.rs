59b29d2336e8287bfd20a3af8e0fc3381e5c44ee
Yagiz Nizipli <yagiz@nizipli.com>
2024-01-07 16:10:44 -0500

    add serde and serde_json

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+mod panic;
 mod traits;
+
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
 
 /// Default severity for diagnostics is `Severity::Error`.
 #[derive(Copy, Clone, Debug, Eq, PartialOrd, PartialEq, Ord)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum Severity {
     Warning,
     Error,
@@ -14,19 +19,49 @@ impl Default for Severity {
     }
 }
 
+#[cfg(feature = "serde")]
+#[test]
+fn test_serialize_severity() {
+    use serde_json::json;
+
+    assert_eq!(json!(Severity::Info), json!("Info"));
+    assert_eq!(json!(Severity::Warning), json!("Warning"));
+    assert_eq!(json!(Severity::Error), json!("Error"));
+}
+
+#[cfg(feature = "serde")]
+#[test]
+fn test_deserialize_severity() {
+    use serde_json::json;
+
+    let severity: Severity = serde_json::from_value(json!("Info")).unwrap();
+    assert_eq!(severity, Severity::Info);
+
+    let severity: Severity = serde_json::from_value(json!("Warning")).unwrap();
+    assert_eq!(severity, Severity::Warning);
+
+    let severity: Severity = serde_json::from_value(json!("Error")).unwrap();
+    assert_eq!(severity, Severity::Error);
+}
+
 /// A span with an optional label.
-/// TODO(@anonrig): Why don't we merge this with SourceSpan and make the label
-/// optional?
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct LabeledSourceSpan {
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     label: Option<String>,
     span: SourceSpan,
 }
 
 /// Represents the start and length of the span.
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct SourceSpan {
     offset: SourceOffset,
     length: usize,
 }
 
 /// Represents the offset from the beginning.
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct SourceOffset(usize);

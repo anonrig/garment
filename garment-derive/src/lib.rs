a7b2dc3dce60895412c434a28b16d47bb5537e3d
Boshen <boshenc@gmail.com>
2024-01-28 22:23:40 +0800

    feat: add derive for `impl Diagnostic for Struct`; add examples/derive.rs

--- garment-derive/src/lib.rs
+++ garment-derive/src/lib.rs
@@ -1,6 +1,16 @@
 use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Diagnostic, attributes(diagnostic))]
-pub fn derive_diagnostic(_input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    quote!().into()
+pub fn derive_diagnostic(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let ident = input.ident;
+
+    let quote = quote! {
+        impl garment::Diagnostic for #ident {
+        }
+    };
+
+    proc_macro::TokenStream::from(quote)
 }
